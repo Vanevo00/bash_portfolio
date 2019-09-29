73a4657a33a094ffd931fece2aa36597f58facfd
Vojtech Vanek <vanevo00@gmail.com>
2019-09-29 10:38:30 +0200

    start of the project, fade in black

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-console.log('It works');
+document.addEventListener('DOMContentLoaded', () => {
+  const firstPage = document.querySelector('#introduction-screen');
+
+  firstPage.classList.add('black-screen');
+});
