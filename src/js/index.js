45419008a4331fdc74db282775810d6adab01d9a
Vojtech Vanek <vanevo00@gmail.com>
2019-09-29 15:17:16 +0200

    welcome page almost done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const firstPage = document.querySelector('#introduction-screen');
+  const introductionScreen = document.querySelector('#introduction-screen');
+  const nano = document.querySelector('#nano');
+  const textContainer = document.querySelector('#text-container');
+  const bottomMenu = document.querySelector('#bottom-menu');
+  const typedTxtFirst = document.querySelector('#typed-txt-first');
+  const decisionText = document.querySelector('#decision-text');
+  const tellHim = document.querySelector('#tell-him');
+  const notSay = document.querySelector('#not-say');
+  const submit = document.querySelector('#submit');
+  const welcomeMessage = document.querySelector('#welcome-message');
+  const welcomeText = document.querySelector('#welcome-text');
 
-  firstPage.classList.add('black-screen');
+  introductionScreen.classList.add('black-screen');
+
+
+  // show the elements on the page in delay
+  setTimeout(() => {
+    nano.classList.remove('opaque');
+    textContainer.classList.remove('opaque');
+    bottomMenu.classList.remove('opaque');
+  },
+  1800);
+
+  // typewriter effect introduction page
+  const firstTypewriterEffect = new Typewriter(typedTxtFirst, {
+    loop: false,
+    delay: 50,
+  });
+
+  firstTypewriterEffect.start()
+    .pauseFor(3000)
+    .typeString('Hi, my name is Vojtech. Welcome to my portfolio site.')
+    .pauseFor(500)
+    .changeDeleteSpeed(10)
+    .deleteChars(30)
+    .pauseFor(400)
+    .typeString(' What is your name?');
+
+  // show decision menu in delay
+  setTimeout(() => {
+    decisionText.classList.remove('opaque');
+  },
+  10000);
+
+  // decision time
+  const showNameWindow = () => {
+    const nameWindow = document.querySelector('#name-window');
+    nameWindow.classList.remove('opaque');
+  };
+
+  tellHim.addEventListener('click', showNameWindow);
+
+  // fade out to next phase and show welcome message
+  const wipeScreen = () => {
+    nano.classList.add('opaque');
+    textContainer.classList.add('opaque');
+    bottomMenu.classList.add('opaque');
+    setTimeout(() => {
+      introductionScreen.style.display = 'none';
+      welcomeMessage.style.display = 'flex';
+    },
+    1500);
+  };
+
+  const welcomeAnonymous = () => {
+    wipeScreen();
+    welcomeText.innerHTML = 'Nice to meet you, anonymous';
+  };
+
+  const welcomeUser = () => {
+    wipeScreen();
+  };
+
+  submit.addEventListener('click', welcomeUser);
+  notSay.addEventListener('click', welcomeAnonymous);
 });
